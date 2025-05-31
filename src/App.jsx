aa9ff2a8a120551f583db8afcbe1986f05580224
leandroffdev <leandrofrancagti@gmail.com>
2025-05-31 20:55:17 +0100

    Criado funcoes de add e delete tasks

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import AddTask from './components/AddTask.jsx';
 import Tasks from './components/Tasks.jsx';
 import { useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
 
 function App() {
 
@@ -38,14 +39,29 @@ function App() {
     setTasks(newTasks);
   }
 
+  const onDeleteTaskClick = (taskId) => {
+    const newTasks = tasks.filter(task => task.id !== taskId);
+    setTasks(newTasks);
+  }
+
+  const onAddTaskSubmit = (title, description) => {
+    const newTask = {
+      id: uuidv4(),
+      title,
+      description,
+      isCompleted: false
+    }
+    setTasks([...tasks, newTask]);
+  }
+
   return (
     <div className="w-screen h-screen bg-slate-500 text-center flex items-center justify-center">
-      <div className='w-[500px]'>
+      <div className='w-[500px] space-y-4'>
         <h1 className='text-red-500text-3xl text-slate-100 font-bold '>
           Gerenciador de tarefas
         </h1>
-        <AddTask />
-        <Tasks tasks={tasks} onTaskClick={onTaskClick}/>
+        <AddTask onAddTaskSubmit={onAddTaskSubmit}/>
+        <Tasks tasks={tasks} onTaskClick={onTaskClick} onDeleteTaskClick={onDeleteTaskClick}/>
       </div>
     </div>
   )
--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -1,6 +1,42 @@
-function AddTask() {
+import { useState } from 'react';
+
+function AddTask({ onAddTaskSubmit }) {
+
+    const[ title, setTitle ] = useState("");
+    const[ description, setDescription ] = useState("");
+
+    console.log(title, description);
+
     return(
-        <h1>AddTask</h1>
+        <div className="space-y-4 p-6 bg-slate-200 rounded-md shadow flex flex-col gap-4">
+            <input 
+                type="text" 
+                placeholder="Digite o titulo da tarefa" 
+                className="border border-slate-300 outline-slate-400 px-4 py-2 rounded-md"
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
+            />
+            <input 
+                type="text" 
+                placeholder="Digite o titulo da tarefa" 
+                className="border border-slate-300 outline-slate-400 px-4 py-2 rounded-md"
+                value={description}
+                onChange={(e) => setDescription(e.target.value)}
+            />
+            <button 
+                onClick={() => {
+                    if (!title.trim() || !description.trim()) {
+                        alert("Preencha todos os campos");
+                        return;
+                    }
+                    onAddTaskSubmit(title, description);
+                    setTitle("");
+                    setDescription("");
+                }}
+                className="bg-slate-500 text-white px-4 py-2 rounded-md font-mediun cursor-pointer">
+                    Adicionar tarefa
+            </button>
+        </div>
     )
 }
 export default AddTask;
